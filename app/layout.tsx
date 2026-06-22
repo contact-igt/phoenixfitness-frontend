087b6114524e2def72c06eb98ceb1d6d6c5722db
Veeravel-S <veeravel.igt@gmail.com>
2026-06-22 19:26:27 +0530

    pixel code added

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,6 +49,22 @@ export default function RootLayout({
           `}
         </Script>
 
+        {/* Meta Pixel */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '148013046383605');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
         {/* GTM noscript */}
         <noscript>
           <iframe
@@ -56,6 +72,18 @@ export default function RootLayout({
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
+        {/* Meta Pixel noscript */}
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=148013046383605&ev=PageView&noscript=1"
+            alt=""
           />
         </noscript>
 
